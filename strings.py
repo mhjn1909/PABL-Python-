ad7a9caa28b1eda77136d03b6e105088a784d835
Harshit Mahajan <harshitmahajan200@gmail.com>
2026-02-04 19:25:36 +0530

    Added strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+#   WAP to demonstrate concept of strings.
+a= input("Enter a string: ")
+print(a)
+b="HelloPython"
+print(b[-5])
+print(b[6])
+"'This is Doc String(only executed if no other command is executed)'"
+
+#Slicing syntax- [Start:Stop:Step]
+f="HelloPython"
+print(f[0:5])  
+print(f[6:])  
+print(f[::2])  
+print(f[::-1]) 
+
+#WAP to reverse str=India
+str="India"
+print(str[::-1])
